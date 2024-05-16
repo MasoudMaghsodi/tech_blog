5dbb8f49a5c778457aeb6d40f59990bf65ec7fbb
MasoudMaghsodi <magsodi333@gmail.com>
2024-05-17 01:57:55 +0330

    test android

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('data'),
+          child: Text('hi'),
         ),
       ),
     );
